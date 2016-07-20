521ce62e17dbac8af80832fa69768e47458a2b8c
Fernando Marins <f.augustomarins@gmail.com>
2016-07-20 14:31:39 -0300

    Cleaned up project

--- MPCRevisited/ChatViewController.swift
+++ MPCRevisited/ChatViewController.swift
@@ -14,11 +14,8 @@ class ChatViewController: UIViewController, UITextFieldDelegate, UITableViewDele
     
     @IBOutlet weak var tblChat: UITableView!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         tblChat.delegate = self
         tblChat.dataSource = self
@@ -28,46 +25,25 @@ class ChatViewController: UIViewController, UITextFieldDelegate, UITableViewDele
 
         txtChat.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
     
     // MARK: IBAction method implementation
-    
     @IBAction func endChat(sender: AnyObject) {
         
     }
     
-    
     // MARK: UITableView related method implementation
-    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0;
+        return 0
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
     
 }
